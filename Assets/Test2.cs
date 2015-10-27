73359250be1a40e6050665bba0305cb6e507e31a
yonek <yonoslaw@gmail.com>
2015-10-27 17:04:24 +0100

    T2 V1

--- Assets/Test2.cs
+++ Assets/Test2.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Test2 : MonoBehaviour {
+public class Test2 : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+  float _Var1;
+
+  // Use this for initialization
+  void Start ()
+  {
 	
-	}
+  }
 	
-	// Update is called once per frame
-	void Update () {
+  // Update is called once per frame
+  void Update ()
+  {
 	
-	}
+  }
 }
