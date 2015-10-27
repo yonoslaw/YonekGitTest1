c438f0be93297cf29347e377c318f09617c1e6f4
yonek <yonoslaw@gmail.com>
2015-10-27 16:41:53 +0100

    V1

--- Assets/Test1.cs
+++ Assets/Test1.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Test1 : MonoBehaviour
 {
 
+  int _Variable1;
+
   // --------------------------------------------------------------------------------------------------------------------------------
 
   void Start ()
